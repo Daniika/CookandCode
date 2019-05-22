dfa652c3dade42f74288381c32ef906fda3f812c
Daniika <46639665+Daniika@users.noreply.github.com>
2019-05-21 21:09:00 -0700

    Add files via upload

---
+++ MadLib.js
@@ -0,0 +1,25 @@
+
+/* Javascript Document */
+
+
+var userName="Wilma";
+var userState="Ohio";
+var userAge=70;
+var userName="Wilma";
+var userState="Ohio";
+var userAge=70;
+var userName="Wilma";
+var userState="Ohio";
+var userAge=70;
+var userName="Wilma";
+var userState="Ohio";
+var userAge=70;
+alert("Greetings ! "+userName+ " You are from "+userState);
+
+
+
+
+
+
+
+
